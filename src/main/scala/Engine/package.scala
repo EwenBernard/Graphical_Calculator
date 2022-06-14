48a14febc5e70700a3aa1e46fa515326b2293dd6
sixtine9 <sixtine.brisard@efrei.net>
2022-06-14 21:30:40 +0200

    show

--- src/main/scala/Engine/package.scala
+++ src/main/scala/Engine/package.scala
@@ -2,9 +2,15 @@ import scala.io.Source
 //import Parser._
 
 package object Engine {
-  val parser = new Parser
-  for (ln <- Source.stdin.getLines)
-    println(parser.parseAll(parser.expr, ln))
+
+  def show() = {
+    val parser = new Parser
+    for (ln <- Source.stdin.getLines)
+      println(parser.parseAll(parser.expr, ln))
+  }
+
 
 
 }
+
+
---
+++ src/main/scala/Engine/test.scala
@@ -0,0 +1,14 @@
+/*package Engine
+
+import scala.io.Source
+
+object test extends App{
+  def run (){
+  val parser = new Parser
+  for (ln <- Source.stdin.getLines)
+    println(parser.parseAll(parser.expr, ln))
+  }
+
+}
+
+ */
